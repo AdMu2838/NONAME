7756f91c9335c2ff97ed647b42f8a4727c48e31b
AdMu2838 <86539821+AdMu2838@users.noreply.github.com>
2023-08-21 10:37:04 -0500

    Revert "Servicios"

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'proyecto.noname',
+  appName: 'proyecto-noname',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,26 +1,16 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  collecion_test: "Servicio",
-  firebaseConfig : {
-    apiKey: "AIzaSyCiE9dk3-rXt1EM6EVLOmL3RAzNjUMiq1A",
-    authDomain: "nonameagustin-c041d.firebaseapp.com",
-    projectId: "nonameagustin-c041d",
-    storageBucket: "nonameagustin-c041d.appspot.com",
-    messagingSenderId: "328481676586",
-    appId: "1:328481676586:web:524a97e6ac00a9d22a5253",
-    measurementId: "G-7PF4JDELBR"
-  },
+  production: false
 };
 
 /*
- * For easier debugging in prodelopment mode, you can import the following file
+ * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
